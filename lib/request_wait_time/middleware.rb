428b501ec2887d40c204a9f2b4a9675798db722f
matejminazek <matej.minazek@infinum.com>
2023-08-29 10:20:57 +0200

    Removed setting RequestWaitTime::Aws::AmznTraceId::HEADER for testing purposes

--- lib/request_wait_time/middleware.rb
+++ lib/request_wait_time/middleware.rb
@@ -5,8 +5,6 @@ module RequestWaitTime
     end
 
     def call(env)
-      #env[RequestWaitTime::Aws::AmznTraceId::HEADER] = "Self=1-64EC7B26-12456789abcdef012345678;Root=1-#{(Time.current.to_i - rand(0..2)).to_s(16)}-abcdef012345678912345678;CalledFrom=app"
-
       amzn_trace_id_header = env[RequestWaitTime::Aws::AmznTraceId::HEADER]
 
       Thread.new { send_metric(amzn_trace_id_header) } if amzn_trace_id_header
--- request_wait_time.gemspec
+++ request_wait_time.gemspec
@@ -32,7 +32,4 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "aws-sdk-cloudwatch", "~> 1.0"
-
-  # For more information and examples about making a new gem, check out our
-  # guide at: https://bundler.io/guides/creating_gem.html
 end
