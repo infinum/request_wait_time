4872c1aeabb4d3f4165f1bb3d2eaffcb9acca7f8
Vedran Hrncic <vedran.hrncic@infinum.hr>
2023-08-29 16:29:46 +0200

    Parse X-Amzn-Trace-Id fields

--- lib/request_queue_time/aws/amzn_trace_id.rb
+++ lib/request_queue_time/aws/amzn_trace_id.rb
@@ -4,15 +4,40 @@ module RequestQueueTime
   module Aws
     class AmznTraceId
       HEADER = 'HTTP_X_AMZN_TRACE_ID'
+      FIELD_DELIMITER = ';'
+      ROOT_FIELD = 'Root'
 
-      def initialize(amzn_trace_id_header)
-        @amzn_trace_id_header = amzn_trace_id_header
+      def initialize(header)
+        @header = header
       end
 
-      attr_reader :amzn_trace_id_header
+      attr_reader :header
 
-      def time
-        amzn_trace_id_header.split('Root=')[1].split('-')[1].to_i(16)
+      def request_start_time
+        root.time.to_i(16)
+      end
+
+      def fields
+        @fields ||= header.split(FIELD_DELIMITER).map { |field| Field.new(field) }
+      end
+
+      def root
+        fields.find { |field| field.name == ROOT_FIELD }
+      end
+    end
+
+    class Field
+      FORMAT_DIVIDER = '='
+      SEGMENT_DELIMITER = '-'
+
+      attr_reader :name
+      attr_reader :version
+      attr_reader :time
+      attr_reader :id
+
+      def initialize(field)
+        @name, segments = field.split(FORMAT_DIVIDER)
+        @version, @time, @id = segments.split(SEGMENT_DELIMITER)
       end
     end
   end
